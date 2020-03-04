70aee59ce82e7b26301b1d75d39e7adad7fc4ed5
root <root@ip-172-31-43-197.us-east-2.compute.internal>
2020-03-04 16:48:10 +0000

    Nandini changes

--- pruneScript.sh
+++ pruneScript.sh
@@ -3,7 +3,7 @@ echo "Please pass in the repo name as parameter"
 
 ##myrep=$1
 #git clone $myrep
-
+echo "Test"
 cd hello-world 
 git branch --remote --merged origin/master | grep -v 'master' |   grep -v 'develop' >> /tmp/mergedBranch
 
